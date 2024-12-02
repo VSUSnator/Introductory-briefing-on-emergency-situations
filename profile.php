ae835d157d265a86b3b29011ed7360ed976fbbe1
Кузнецов Владислав Дмитриевич <kuznecovvd@clinic.local>
2024-12-02 08:48:54 +0800

    пересобрал профиль

--- profile.php
+++ profile.php
@@ -110,138 +110,139 @@ if ($userPhoneNumber) {
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" type="text/css" href="style.css" media="all" />
     <style>
-        body {
-            font-family: 'Arial', sans-serif;
-            margin: 0;
-            padding: 20px;
-            background-color: #f7f9fc; /* Светлый серо-голубой фон */
-        }
-        .logo {
-            text-align: center; /* Центрируем логотип */
-            margin: 20px 0; /* Отступы сверху и снизу */
-        }
-        .logo img {
-            max-width: 100%; /* Делает изображение адаптивным */
-            height: auto; /* Сохраняет пропорции изображения */
-        }
-        #menu {
-            background: #5e35b1; /* Фиолетовый фон для меню */
-            box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.3);
-            margin-bottom: 20px; /* Отступ снизу для меню */
-        }
-        .menu ul {
-            list-style-type: none; /* Убираем маркеры списка */
-            padding: 0; /* Убираем отступы */
-            margin: 0; /* Убираем отступы */
-            text-align: center; /* Центрируем текст меню */
-        }
-        .menu ul li {
-            display: inline; /* Выравнивание элементов в строку */
-            margin: 0 0px; /* Отступы между элементами меню */
-        }
-        .menu ul li a {
-            color: white; /* Цвет текста в меню */
-            text-decoration: none; /* Убираем подчеркивание */
-            transition: color 0.3s; /* Плавный переход цвета */
-        }
-        .menu ul li a:hover {
-            color: #ab47bc; /* Цвет текста при наведении */
-        }
-        #wrapper {
-            background-color: white; /* Белый фон для обертки */
-            padding: 30px;
-            border-radius: 8px;
-            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* Меньшая тень для обертки */
-            margin-top: 20px;
-        }
-        .intro-instructions h2 {
-            color: #5e35b1; /* Более мягкий фиолетовый цвет */
-            margin-bottom: 15px;
-            font-size: 24px;
-            border-bottom: 2px solid #ab47bc; /* Светлый фиолетовый подчеркиватель */
-            padding-bottom: 10px;
-        }
-        #userInfo {
-            margin-bottom: 20px;
-        }
-        #materialList {
-            margin-top: 20px;
-        }
-        .material-link {
-            color: #3B0FFF; /* Цвет текста ссылки */
-            text-decoration: none; /* Убираем подчеркивание */
-            transition: color 0.3s; /* Плавный переход цвета */
-        }
-        .material-link:hover {
-            color: #ab47bc; /* Цвет текста при наведении */
-            text-decoration: underline; /* Подчеркивание при наведении */
-        }
-        #materialList p {
-            font-size: 16px;
-            margin: 10px 0;
-            padding: 10px;
-            border: 1px solid #d1d1d1; /* Светлая рамка */
-            border-radius: 5px;
-            background-color: #f4f4f4; /* Светло-серый фон для материалов */
-            transition: background-color 0.3s, color 0.3s;
-        }
-        #materialList p:hover {
-            background-color: #e0e0e0; /* Более темный светло-серый фон при наведении */
-            color: #333; /* Темный текст при наведении */
-        }
-        button {
-            background-color: #45a049; /* Яркий оранжевый цвет для кнопок */
-            color: white;
-            border: none;
-            padding: 15px 20px;
-            border-radius: 5px;
-            cursor: pointer;
-            font-size: 18px;
-            transition: background-color 0.3s, transform 0.2s, box-shadow 0.3s; /* Добавлен переход для тени */
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Тень для кнопок */
-        }
-        button:hover {
-            background-color: #388e3c; /* Темнее оранжевый при наведении */
-            transform: translateY(-2px); /* Поднятие кнопки при наведении */
-            box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3); /* Увеличение тени при наведении */
-        }
-        #message {
-            margin: 20px 0;
-            padding: 10px;
-            border-radius: 5px;
-            background-color: #e9ecef; /* Светлый фон для сообщений */
-            font-size: 16px;
-            color: #3B0FFF; /* Синий текст для сообщений */
-        }
-        #errorMessage {
-            color: red;
-            font-weight: bold;
-            padding: 10px;
-            border: 1px solid red;
-            border-radius: 5px;
-            background-color: #f8d7da; /* Светлый фон для сообщений об ошибках */
-        }
-        #materialRedirectButton {
-            background-color: #007bff; /* Основной фиолетовый цвет для кнопки перенаправления */
-            margin-top: 20px; /* Отступ сверху для кнопки перенаправления */
-        }
-        #materialRedirectButton:hover {
-            background-color: #0056b3; /* Темный фиолетовый при наведении */
-        }
-        .special-materials {
+    body {
+        font-family: 'Arial', sans-serif;
+        margin: 0;
+        padding: 20px;
+        background-color: #f7f9fc; /* Светлый серо-голубой фон */
+    }
+    .logo {
+        text-align: center; /* Центрируем логотип */
+        margin: 20px 0; /* Отступы сверху и снизу */
+    }
+    .logo img {
+        max-width: 100%; /* Делает изображение адаптивным */
+        height: auto; /* Сохраняет пропорции изображения */
+    }
+    #menu {
+        background: #5e35b1; /* Фиолетовый фон для меню */
+        box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.3);
+        margin-bottom: 20px; /* Отступ снизу для меню */
+    }
+    .menu ul {
+        list-style-type: none; /* Убираем маркеры списка */
+        padding: 0; /* Убираем отступы */
+        margin: 0; /* Убираем отступы */
+        text-align: center; /* Центрируем текст меню */
+    }
+    .menu ul li {
+        display: inline; /* Выравнивание элементов в строку */
+        margin: 0 0px; /* Отступы между элементами меню */
+    }
+    .menu ul li a {
+        color: white; /* Цвет текста в меню */
+        text-decoration: none; /* Убираем подчеркивание */
+        transition: color 0.3s; /* Плавный переход цвета */
+    }
+    .menu ul li a:hover {
+        color: #ab47bc; /* Цвет текста при наведении */
+    }
+    #wrapper {
+        background-color: white; /* Белый фон для обертки */
+        padding: 30px;
+        border-radius: 8px;
+        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); /* Меньшая тень для обертки */
+        margin-top: 20px;
+    }
+    .intro-instructions h2 {
+        color: #5e35b1; /* Более мягкий фиолетовый цвет */
+        margin-bottom: 15px;
+        font-size: 24px;
+        border-bottom: 2px solid #ab47bc; /* Светлый фиолетовый подчеркиватель */
+        padding-bottom: 10px;
+    }
+    #userInfo {
+        margin-bottom: 20px;
+    }
+    #materialList {
+        margin-top: 20px;
+    }
+    .material-link {
+        color: #3B0FFF; /* Цвет текста ссылки */
+        text-decoration: none; /* Убираем подчеркивание */
+        transition: color 0.3s; /* Плавный переход цвета */
+    }
+    .material-link:hover {
+        color: #ab47bc; /* Цвет текста при наведении */
+        text-decoration: underline; /* Подчеркивание при наведении */
+    }
+    #materialList p {
+        font-size: 16px;
+        margin: 10px 0;
+        padding: 10px;
+        border: 1px solid #d1d1d1; /* Светлая рамка */
+        border-radius: 5px;
+        background-color: #f4f4f4; /* Светло-серый фон для материалов */
+        transition: background-color 0.3s, color 0.3s;
+    }
+    #materialList p:hover {
+        background-color: #e0e0e0; /* Более темный светло-серый фон при наведении */
+        color: #333; /* Темный текст при наведении */
+    }
+    button {
+        background-color: #45a049; /* Яркий оранжевый цвет для кнопок */
+        color: white;
+        border: none;
+        padding: 15px 20px;
+        border-radius: 5px;
+        cursor: pointer;
+        font-size: 18px;
+        transition: background-color 0.3s, transform 0.2s, box-shadow 0.3s; /* Добавлен переход для тени */
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Тень для кнопок */
+    }
+    button:hover {
+        background-color: #388e3c; /* Темнее оранжевый при наведении */
+        transform: translateY(-2px); /* Поднятие кнопки при наведении */
+        box-shadow: 0 6px 12px rgba(0, 0, 0, 0.3); /* Увеличение тени при наведении */
+    }
+    #message {
+        margin: 20px 0;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: #e9ecef; /* Светлый фон для сообщений */
+        font-size: 16px;
+        color: #3B0FFF; /* Синий текст для сообщений */
+    }
+    #errorMessage {
+        color: red;
+        font-weight: bold;
+        padding: 10px;
+        border: 1px solid red;
+        border-radius: 5px;
+        background-color: #f8d7da; /* Светлый фон для сообщений об ошибках */
+    }
+    #materialRedirectButton {
+        background-color: #007bff; /* Основной фиолетовый цвет для кнопки перенаправления */
+        margin-top: 20px; /* Отступ сверху для кнопки перенаправления */
+    }
+    #materialRedirectButton:hover {
+        background-color: #0056b3; /* Темный фиолетовый при наведении */
+    }
+    .special-materials {
         border: 2px solid #007BFF; /* Синяя рамка */
         background-color: #E7F3FF; /* Светло-синий фон */
         padding: 15px;
         margin-top: 20px;
         border-radius: 5px;
-        }
-        .special-materials h4 {
-         color: #0056b3; /* Темно-синий цвет заголовка */
-        }
-        .special-materials a {
-         font-weight: bold; /* Выделение ссылок */
-        }
-    </style>
+    }
+    .special-materials h4 {
+        color: #0056b3; /* Темно-синий цвет заголовка */
+    }
+    .special-materials a {
+        font-weight: bold; /* Выделение ссылок */
+    }
+
+</style>
 
 </head>
 <body>
@@ -274,45 +275,55 @@ if ($userPhoneNumber) {
         </div>
         <h3>Материалы для прохождения</h3>
         <div id="materialList">
-    <?php if (isset($_SESSION['user'])): ?>
-        <?php
-        // Общие материалы
-        $mainMaterials = [
-            "Гражданская оборона и ЧС",
-            "Антитеррористическая защищенность",
-            "Порядок действия сотрудников при пожаре"
-        ];
+            <?php if (isset($_SESSION['user'])): ?>
+                <?php
+                // Общие материалы
+                $mainMaterials = [
+                    "Гражданская оборона и ЧС",
+                    "Антитеррористическая защищенность",
+                    "Порядок действия сотрудников при пожаре"
+                ];
 
-        // Выводим основные материалы
-        foreach ($mainMaterials as $index => $title): ?>
-            <p>
-                <a href="blog<?php echo $index + 1; ?>.html" class="material-link"><?php echo $title; ?>: <strong><?php echo $_SESSION['user']['completed_materials']['main']['material' . ($index + 1)] ? 'Пройдено' : 'Не пройдено'; ?></strong></a>
-            </p>
-        <?php endforeach; ?>
+                // Выводим основные материалы
+                foreach ($mainMaterials as $index => $title): ?>
+                    <p>
+                        <a href="blog<?php echo $index + 1; ?>.html" class="material-link"><?php echo $title; ?>: <strong><?php echo $_SESSION['user']['completed_materials']['main']['material' . ($index + 1)] ? 'Пройдено' : 'Не пройдено'; ?></strong></a>
+                    </p>
+                <?php endforeach; ?>
 
-        <?php
-        // Дополнительные материалы
-        $userProfession = $_SESSION['user']['profession'];
-        $userAdditionalMaterials = $additionalMaterials[$userProfession] ?? []; // Получаем материалы для данной профессии
-        $userAdditionalMaterialsLinks = $additionalMaterialsLinks[$userProfession] ?? []; // Получаем ссылки для данной профессии
+                <?php
+                // Дополнительные материалы
+                $userProfession = $_SESSION['user']['profession'];
+                $userAdditionalMaterials = $additionalMaterials[$userProfession] ?? []; // Получаем материалы для данной профессии
+                $userAdditionalMaterialsLinks = $additionalMaterialsLinks[$userProfession] ?? []; // Получаем ссылки для данной профессии
 
-        // Выводим дополнительные материалы
-        if (!empty($userAdditionalMaterials)) {
-            echo '<h3>Дополнительные материалы</h3>';
-            foreach ($userAdditionalMaterials as $index => $title): ?>
-                <p>
-                    <a href="<?php echo htmlspecialchars($userAdditionalMaterialsLinks[$index]); ?>" class="material-link">
-                        <?php echo htmlspecialchars($title); ?>: 
-                        <strong><?php echo $_SESSION['user']['completed_materials']['main']['material' . (count($mainMaterials) + $index + 1)] ? 'Пройдено' : 'Не пройдено'; ?></strong>
-                    </a>
-                </p>
-            <?php endforeach; 
-        }
-        ?>
-    <?php else: ?>
-        <p>Материалы доступны только зарегистрированным пользователям.</p>
-    <?php endif; ?>
-</div>
+                // Выводим дополнительные материалы
+                if (!empty($userAdditionalMaterials)) {
+                    echo '<h3>Дополнительные материалы</h3>';
+                    foreach ($userAdditionalMaterials as $index => $title): 
+                        $isCompleted = $_SESSION['user']['completed_materials']['main']['material' . (count($mainMaterials) + $index + 1)];
+                        ?>
+                        <p class="<?php echo !$isCompleted ? 'not-completed' : ''; ?>">
+                            <a href="<?php echo htmlspecialchars($userAdditionalMaterialsLinks[$index]); ?>" class="material-link">
+                                <?php echo htmlspecialchars($title); ?>: 
+                                <strong><?php echo $isCompleted ? 'Пройдено' : 'Не пройдено'; ?></strong>
+                            </a>
+                        </p>
+                    <?php endforeach; 
+                }
+                ?>
+            <?php else: ?>
+                <p>Материалы доступны только зарегистрированным пользователям.</p>
+            <?php endif; ?>
+
+            <?php if (isset($_SESSION['user'])): ?>
+            <!-- Кнопка "Начать обучение" -->
+            <div>
+                <a href="registrationForm.html" class="button">Начать обучение</a>
+            </div>
+        <?php endif; ?>
+        </div>
+        <button id="nextButton" style="display:none;" aria-label="Перейти на следующий сайт">Перейти на следующий сайт</button>
     </section>
 </div>
 
@@ -332,6 +343,21 @@ if ($userPhoneNumber) {
         }
     });
 </script>
-
 </body>
 </html>
+
+<!-- <body>
+<h3>Дополнительные материалы</h3>
+<div class="special-materials">
+    <?php foreach ($userAdditionalMaterials as $index => $title): 
+        $isCompleted = $_SESSION['user']['completed_materials']['main']['material' . (count($mainMaterials) + $index + 1)];
+        ?>
+        <p class="<?php echo !$isCompleted ? 'not-completed' : ''; ?>">
+            <a href="<?php echo htmlspecialchars($userAdditionalMaterialsLinks[$index]); ?>" class="material-link">
+                <?php echo htmlspecialchars($title); ?>: 
+                <strong><?php echo $isCompleted ? 'Пройдено' : 'Не пройдено'; ?></strong>
+            </a>
+        </p>
+    <?php endforeach; ?>
+</div>
+</body> -->
